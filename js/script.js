ae28f9f9ebd9ca11ccc4ad9e78b9339b1a2bfeac
Michael_J_T <lightbar200@gmail.com>
2024-02-14 19:11:31 -0600

    added objects and methods

--- js/script.js
+++ js/script.js
@@ -2,9 +2,35 @@ const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
 
 const createPet = function (name, species) {
-    const pet {
+    const pet = {
         name: name,
         species: species,
-        isTired: Number(5),
-    }
+        isTired: 5,
+        sleep : function () {
+            console.log(`${this.name} needs a nap. Zzz... `)
+            this.isTired= 1;
+        },
+        play: function () {
+            if (this.isTired === 10) {
+                console.log("too tired to play.")
+                this.sleep ()
+            } else {
+                console.log(`Yay! ${name} loves to play!`)
+                this.isTired += 1;
+            }
+        }
+    };
+    return pet;
 }
+const sora = createPet("sora", "ferret")
+const clover = createPet("Clover", "rabbit")
+const baxter = createPet("Baxter", "hamster")
+const cleo = createPet("cleo", "rat")
+const francine = createPet("Francine", "turtle") 
+
+//console.log(sora, clover,baxter,cleo,francine)
+
+clover.sleep();
+baxter.play();
+
+//console.log(clover,baxter)
